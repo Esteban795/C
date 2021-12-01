762c91a2497a2e714b0e549cacd5e02f67c93526
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2021-12-01 12:58:47 +0100

    almost done

--- td16/exo1.c
+++ td16/exo1.c
@@ -175,7 +175,7 @@ void delete(int_dynarray* t) {
 void print(int_dynarray* t){
     int n = length(t);
     for (int i = 0; i < n; i++){
-        printf("%d ", get(t, i));
+        printf(" %d ", get(t, i));
     }
     printf("\n");
 }
@@ -184,34 +184,95 @@ void print(int_dynarray* t){
 
 /* Partie 3.2 */
 
-void push(int_dynarray* t, int x);
+void push(int_dynarray* t, int x) {
+    int temp_capacity = (*t).capacity;
+    if ((*t).len == temp_capacity) {
+        if (temp_capacity == 0) {
+            resize(t,1);
+        } else {
+            resize(t,temp_capacity * 2);
+        }
+    }
+    (*t).len++;
+    set(t,(*t).len -1,x); 
+}
 
-int pop(int_dynarray* t);
-
+int pop(int_dynarray* t) {
+    assert((*t).len > 0);
+    int x = (*t).data[(*t).len - 1];
+    (*t).len--;
+    if (2 * (*t).len < (*t).capacity) {
+        resize(t,(*t).capacity / 2);
+    }
+    return x;
+}
 
 
 /* Partie 3.3 */
 
-void insert_at(int_dynarray* t, int i, int x);
+void insert_at(int_dynarray* t, int i, int x) {
+    int l = length(t);
+    assert(i < length(t) && i >= 0);
+    push(t,x);
+    for (int j = l; j > i;j--) {
+        set(t,j,get(t,j - 1));
+    }
+    set(t,i,x);
+}
 
-int pop_at(int_dynarray* t, int i);
+int pop_at(int_dynarray* t, int i) {
+    int l = length(t);
+    assert(i < length(t) && i >= 0);
+    int temp = get(t,i);
+    for (int k = i; k < l - 1; k++) {
+        set(t, k,get(t,k + 1));
+    }
+    (*t).len--;
+    return temp;
+}
 
-int position_linear(int_dynarray* t, int x);
+int position_linear(int_dynarray* t, int x) {
+    int i = length(t) - 1;
+    while (i >= 0 && get(t,i) > x) {
+        i--;
+    }
+    return i + 1;
+}
 
 int position(int_dynarray* t, int x);
 
-int_dynarray* insertion_sort(int_dynarray* t);
+int_dynarray* insertion_sort(int_dynarray* t) {
+    int_dynarray* out = make_empty();
+    int len = length(t);
+    for (int i = 0; i < len;i++) {
+        int elt = get(t,i);
+        int index = position_linear(out,elt);
+        insert_at(t,index,elt);
+    }
+    return out;
+}
 
 
 
 
 int main(void){
     int_dynarray* dyntab = make_empty();
-    push_naif(dyntab,1);
-    push_naif(dyntab,2);
-    push_naif(dyntab,3);
-    int temp = pop_naif(dyntab);
+    push(dyntab,3);
+    push(dyntab,2);
+    push(dyntab,1);
+    print(dyntab);
+    int temp = pop(dyntab);
     printf("Temp : %d\n",temp);
+    insert_at(dyntab,1,4);
+    print(dyntab);
+    int temp2 = pop_at(dyntab,1);
+    printf("Temp2 (pop_at : %d) : %d\n",1,temp2);
+    printf("\n\n");
+    print(dyntab);
+    printf("\n\n");
+    int_dynarray* sorted_tab = insertion_sort(dyntab);
+    printf("Len sorted tab : %d",length(sorted_tab));
     delete(dyntab);
+    delete(sorted_tab); 
     return 0;
 }
