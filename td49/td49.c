4e0c7db35275a5523e093dd97f3c006808179fdf
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2022-06-01 21:30:32 +0200

    td49

--- td49/td49.c
+++ td49/td49.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
+typedef int litteral;
 
-//Q8
+struct cnf {
+    litteral** clauses;
+    int nb_clauses;
+    int nb_variables;
+};
 
+typedef int sat;
+typedef struct cnf cnf;
+
+const sat SAT = 0;
+const sat UNSAT = 1;
+const sat UNKNOWN = 2;
+
+sat remove_clause(cnf* f,int i){
+    f->nb_clauses--;
+    
+}
