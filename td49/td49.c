f1e22059d4f28d49410051731e81094cb76ea326
Esteban <estebanmandjukowski@gmail.com>
2022-06-03 16:49:50 +0200

    td49

--- td49/td49.c
+++ td49/td49.c
@@ -18,6 +18,28 @@ const sat UNSAT = 1;
 const sat UNKNOWN = 2;
 
 sat remove_clause(cnf* f,int i){
+    if (f->nb_clauses < 1) return;
+    f->clauses[i][0] = 0; //clause non active
     f->nb_clauses--;
-    
+    if (f->nb_variables = 0) return SAT;
+    else return UNKNOWN;
+}
+
+sat remove_litteral(litteral clause[],int index){
+    clause[index] = 0;
+    if (clause[0] = 0) return UNSAT;
+    else return UNKNOWN;
+}
+
+int get_litteral_occurence(litteral clause[], litteral l){
+    int count = 0;
+    int n = clause[0];
+    for (int i = 1; i < n; i++){
+        if (clause[i] == l || clause[i] == -l) count++;
+    }
+    return count;
+}
+
+sat assert_litteral(cnf* f,litteral valuation[],litteral x){
+    int count = 0;
 }
