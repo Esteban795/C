99703560cd7ae0e24eed1b734776662272b22118
Esteban <estebanmandjukowski@gmail.com>
2022-12-07 23:26:44 +0100

    global_list file done (tp13)

--- 2A/tp13_quicksort_mt/global_list.c
+++ 2A/tp13_quicksort_mt/global_list.c
@@ -19,12 +19,36 @@ typedef struct global_data global_data_t;
 
 
 static void process_task(global_data_t *data, slice_t slice){
-    if ()
+    if (slice.len < MIN_SIZE){
+        full_sort(slice.start,slice.len);
+        decrement(data->nb_tasks);
+        return;
+    } else {
+        int pivot = partition(slice.start,slice.len);
+        slice_t s_lo = {.start = slice.start,.len = pivot};
+        slice_t s_hi = {.start = slice.start + pivot + 1,.len = slice.len - pivot - 1};
+        increment(data->nb_tasks);
+        if (s_lo.len < s_hi.len){
+            push_right(data->queue,s_hi);
+            process_task(data,s_lo);
+        } else {
+            push_right(data->queue,s_lo);
+            process_task(data,s_hi);
+        }
+    }
 }
 
 
-static void *worker(void *args);
-
+static void* worker(void *args){
+    global_data_t* data = (global_data_t*)args;
+    while (get_counter(data->nb_tasks) != 0){
+        slice_t t;
+        if (try_pop_left(data->queue,&t)) {
+            process_task(data,t);
+        }
+    }
+    return NULL;
+}
 
 void parallel_sort_global(int *arr, int len, int nb_threads){
     pthread_t threads[nb_threads];
