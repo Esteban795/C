9dfebf5fe8cd36ca5d4b333e3c44e47e75141acb
Esteban <estebanmandjukowski@gmail.com>
2023-01-20 21:04:19 +0100

    tp16 halfway done

--- 2A/tp17_ExactCover/squelette-dense.c
+++ 2A/tp17_ExactCover/squelette-dense.c
@@ -85,29 +85,31 @@ bool get(Dense *inst, Partial *partial, int i, int j){
 
 // Exercice 4
 
-bool **read_matrix(int *n, int *p){
+bool **read_matrix(FILE* f,int *n, int *p){
     bool** m = malloc(*n * sizeof(bool*));
     int temp;
     for (int i = 0; i < *n;i++){
         m[i] = malloc(*p * sizeof(bool));
         for (int j = 0; j < *p;j++){
-            scanf("%d ",&temp);
+            fscanf(f,"%d",&temp);
             m[i][j] = temp == 1 ? true: false;
         }
     }
     return m;
 }
 
-Dense *read_instance(void){
+
+Dense *read_instance(FILE* f){
     Dense* instance = malloc(sizeof(Dense));
     int n;
     int p;
-    fscanf("%d %d\n",&n,&p);
+    fscanf(f,"%d %d\n",&n,&p);
     instance->nb_columns = p;
     instance->nb_rows = n;
-    instance->data = read_matrix(&n,&p);
+    instance->data = read_matrix(f,&n,&p);
     return instance;
 }
+
 void print_partial_instance(Dense *inst, Partial *partial){
     int n = inst->nb_rows;
     int p = inst->nb_columns;
@@ -115,7 +117,7 @@ void print_partial_instance(Dense *inst, Partial *partial){
     for (int j = 0; j < p;j++){
         if (partial->remaining_columns[j]) {
             for (int i = 0; i < n;i++){
-                printf("%d ",inst->data[i][j]);
+                printf("%d",inst->data[i][j]);
             }
             printf("\n");
         }
@@ -140,24 +142,32 @@ void print_partial_solution(Dense *inst, Partial *partial){
     printf("\n");
 }
 
+void print_m(bool** m,int n,int p){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < p;j++){
+            printf("%d ",m[i][j] == 1 ? 1 : 0);
+        }
+        printf("\n");
+    }
+}
 
 // Exercice 5
 
 void cover_column(Dense *inst, Partial *partial, int r, int c){
     for (int i = 0; i < inst->nb_rows;i++){
-        if (inst->data[i][r] && i != r) partial->remaining_rows[i] = false;
+        if (!get(inst,partial,i,c) || i == r) continue;
+        partial->remaining_rows[i] = false;
     }
     partial->remaining_columns[c] = false;
 }
 
 void select_row(Dense *inst, Partial *partial, int r){
-    for (int j = 0; j < inst->nb_rows; j++){
-        if (partial->remaining_columns[j]){
-            cover_column(inst,partial,r,j);
-        }
+    partial->selected_rows[r] = true;
+    for (int j = 0; j < inst->nb_columns; j++){
+        if (!get(inst,partial,r,j)) continue;
+        cover_column(inst,partial,r,j);
     }
     partial->remaining_rows[r] = false;
-    partial->selected_rows[r] = true;
 }
 
 
@@ -174,25 +184,13 @@ int counter_true(bool* arr,int len){
 
 int choose_first_column(Dense *inst, Partial *partial){
     for (int c = 0; c < inst->nb_columns;c++){
-        if (!partial->remaining_rows[c]) return c;
+        if (partial->remaining_columns[c]) return c;
     }
+    return -1;
 }
 
-int choose_column_min(Dense* inst,Partial* partial){
-    int min_cardinal = 1 << 20;
-    int chosen_column = choose_first_column(inst,partial);
-    for (int j = 0; j < inst->nb_columns;j++){
-        int nb = 0;
-        for (int i = 0; i < inst->nb_rows;i++){
-            if (partial->selected_rows[r] = true;) nb++;
-        }
-        if (nb < min_cardinal) {
-            min_cardinal = nb;
-            chosen_column = j;
-        }
-    }
-    return chosen_column;
-}
+
+
 int count(Dense *inst, Partial *partial){
     int n = inst->nb_rows;
     int p = inst->nb_columns;
@@ -201,14 +199,47 @@ int count(Dense *inst, Partial *partial){
     int c = choose_first_column(inst,partial);
     int nb_sol = 0;
     for (int i = 0; i < n;i++){
-        if ()
+        if (get(inst,partial,i,c)){
+            Partial* copy_partial = partial_copy(partial,n,p);
+            select_row(inst,copy_partial,i);
+            nb_sol += count(inst,copy_partial);
+            partial_free(copy_partial);
+        }
     }
+    return nb_sol;
 }
 
 
-bool search(Dense *inst, Partial *partial, Partial **solution);
+bool search(Dense *inst, Partial *partial, Partial **solution){
+    int n = inst->nb_rows;
+    int p = inst->nb_columns;
+    if (counter_true(partial->remaining_columns,p) == 0) {
+        *solution = partial_copy(partial,n,p);
+        return true;
+    }
+    if (counter_true(partial->remaining_rows,n) == 0) return false;
+    int c = choose_first_column(inst,partial);
+    for (int i = 0; i < n;i++){
+        if (!get(inst,partial,i,c)) continue;
+        Partial* copy_partial = partial_copy(partial,n,p);
+        select_row(inst,partial,i);
+        bool res = search(inst,partial,solution);
+        partial_free(copy_partial);
+        if (res) return true; //on ne veut pas sortir de la boucle direct si on ne trouve pas de solutions du premier coup...
+    }
+    return false;
+}
 
 
 int main(int argc, char **argv){
+    if (argc != 2) return EXIT_FAILURE;
+    FILE* f = fopen(argv[1],"r");
+    Dense* inst = read_instance(f);
+    fclose(f);
+    Partial* part = partial_new(inst);
+    int nb = count(inst,part);
+    dense_free(inst);
+    partial_free(part);
+    printf("Nombre de solutions : %d\n",nb);
     return 0;
 }
