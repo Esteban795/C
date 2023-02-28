cdac35d62d613953ff489aaf078909413a0af96e
Esteban <estebanmandjukowski@gmail.com>
2023-02-28 19:58:23 +0100

    tp steganographie done

--- 2A/tp_steganographie/tp24.c
+++ 2A/tp_steganographie/tp24.c
@@ -5,8 +5,8 @@
 #include <assert.h>
 
 struct image {
-  int width;
-  int height;
+  int larg;
+  int haut;
   int maxc;
   uint16_t **pixels;
 };
@@ -16,90 +16,154 @@ typedef struct image image;
 /** Fabrique une image à partir du contenu d'un fichier supposé au
     format PGM ASCII */
 image *charger_image(char *nom_fichier) {
-  image* img = malloc(sizeof(image));
-  FILE* f = fopen(nom_fichier,"r");
-  fscanf(f,"P2");
-  int width,height;
-  fscanf(f,"%d %d\n",&width,&height);
-  uint16_t** pixels = malloc(sizeof(uint16_t*) * height);
-  int temp;
-  uint16_t temp16;
-  for (int i = 0; i < height;i++){
-    pixels[i] = malloc(sizeof(uint16_t) * width);
-    for (int j = 0; j < width; j++){
-      fscanf(f,"%d ",&temp);
-      if (temp <= 65535 && temp > 0){
-        temp16 = (uint16_t)temp;
-        pixels[i][j] = temp;
-      } else {
-        return NULL;
-      }
-    }
-    fscanf(f,"\n");
-  }
-  img->pixels = pixels;
-  img->height = height;
-  img->width = width;
-  img->maxc = 65535;
-  fclose(f);
-  return img;
-}
 
-/** Libère une image */
-void liberer_image(image *img) {
-  for (int i = 0; i < img->height; i++) {
-    free(img->pixels[i]);
+  FILE *fichier = fopen(nom_fichier, "r");
+  if (fichier == NULL) {
+    return NULL;
   }
-  free(img->pixels);
-  free(img);
+
+  // Vérifie que le format commence bien par P2
+  char c = '\0';
+  fscanf(fichier, "%c", &c);
+  assert(c == 'P');
+  fscanf(fichier, "%c", &c);
+  assert(c == '2');
+
+  image *img = malloc(sizeof(image));
+
+  fscanf(fichier, "%d", &img->larg);
+  fscanf(fichier, "%d", &img->haut);
+  fscanf(fichier, "%d", &img->maxc);
+
+  img->pixels = malloc(img->haut * sizeof(uint16_t*));
+  for (int i = 0; i < img->haut; i++) {
+    img->pixels[i] = malloc(img->larg * sizeof(uint16_t));
+    for (int j = 0; j < img->larg; j++) {
+      int p = 0;
+      fscanf(fichier, "%d", &p);
+      assert(0 <= p && p <= 65536);
+      img->pixels[i][j] = (uint16_t)p;
+    }
+  }
+  fclose(fichier);
+
+  return img;
+
 }
 
 /** Renvoie le caractère caché dans les `8` premières cases d'un
     tableau supposé de longueur au moins `8` */
 char caractere(uint16_t *tab) {
-  int temp = 0;
-  for (int i = 0; i < 8;i++){
-    int bit = tab[i] & 1;
-    temp += bit * (1 << (8 - i));
+  int res = 0;
+  for (int j = 0; j < 8; j++) {
+    res *= 2;
+    res += tab[j] % 2;
   }
-  return (char)temp;
+  return (char)res;
 }
 
 /** Écrit dans le flux le message caché dans l'image. On suppose que
     l'image, et le flux sont valides, et en particulier non `NULL`. On
     suppose également que le message est valide et donc, en
     particuler, que `img->larg >= 8`. */
-int sauvegarder_message(image *img, char *nom_sortie){
-  FILE* output = fopen(nom_sortie,"w");
-  if (output == NULL) return 1;
-  int start = 0;
-  int nb_chars_per_line = img->width / 8;
-  for (int i = 0; i < img->height; i++){
-    for (int j = 0; j < nb_chars_per_line;j++){
-      char c = caractere(img->pixels[i] + start);
-      fprintf(output,"%c",c);
-    }
+int sauvegarder_message(image *img, char *nom_sortie) {
+  FILE *sortie = fopen(nom_sortie, "w");
+  if (sortie == NULL) {
+    fprintf(stderr, "Impossible d'ouvrir le fichier %s en écriture\n", nom_sortie);
+    return 1;
   }
-  fclose(output);
+  for (int i = 0; i < img->haut; i++) {
+    char c = caractere(img->pixels[i]);
+    if (c == '\0') {
+      break;
+    }
+    fprintf(sortie, "%c", c);
+  }
+  fclose(sortie);
   return 0;
 }
 
 /** Insère le caractère dans les `8` premières cases du tableau. On
     suppose que le tableau est de taille suffisante. */
 void inserer_caractere(char c, uint16_t *tab) {
+  int n = (int)c;
+  for (int j = 7; j >= 0; j--) {
+    tab[j] = 2 * (tab[j] / 2) + n % 2;
+    n /= 2;
+  }
 }
 
 /** Cache un message dans une image. On suppose que l'image est de
     hauteur et de largeur suffisante. */
 int cacher(image *img, char *nom_entree) {
-  return 1;
+  FILE *entree = fopen(nom_entree, "r");
+  if (entree == 0) {
+    fprintf(stderr, "Impossible d'ouvrir le fichier %s en lecture\n", nom_entree);
+    return 1;
+  }
+  assert(img->larg >= 8);
+  int i = 0;
+  while (!feof(entree)) {
+    assert(i < img->haut);
+    char c = '\0';
+    fscanf(entree, "%c", &c);
+    inserer_caractere(c, img->pixels[i]);
+    i++;
+  }
+  fclose(entree);
+  return 0;
 }
 
 /** Sauvegarde une image dans un fichier au format PGM ASCII. On
     suppose que l'image est valide. */
 int sauvegarder_image(image *img, char *nom_fichier) {
-  return 1;
+  FILE *fichier = fopen(nom_fichier, "w");
+  if (fichier == NULL) {
+    fprintf(stderr, "Impossible d'ouvrir le fichier %s en écriture\n", nom_fichier);
+    return 1;
+  }
+  fprintf(fichier, "P2\n");
+  fprintf(fichier, "%d %d\n", img->larg, img->haut);
+  fprintf(fichier, "%d\n", img->maxc);
+  for (int i = 0; i < img->haut; i++) {
+    for (int j = 0; j < img->larg; j++) {
+      fprintf(fichier, "%d", (int)img->pixels[i][j]);
+      fprintf(fichier, "%c", j < img->larg - 1 ? ' ' : '\n');
+    }
+  }
+  return 0;
+}
+
+void liberer_image(image *img) {
+  for (int i = 0; i < img->haut; i++) {
+    free(img->pixels[i]);
+  }
+  free(img->pixels);
+  free(img);
+}
+
+void masquer(image *dst, image *src) {
+  assert(dst->larg == src->larg && dst->haut == src->haut && dst->maxc == src->maxc);
+  const uint16_t mask = 0xFF00;
+  for (int i = 0; i < src->haut; i++) {
+    for (int j = 0; j < src->larg; j++) {
+      dst->pixels[i][j] &= mask;
+      dst->pixels[i][j] += ~mask & (src->pixels[i][j] >> 8);
+    }
+  }
+}
+
+void demasquer(image *img) {
+  for (int i = 0; i < img->haut; i++) {
+    for (int j = 0; j < img->larg; j++) {
+      img->pixels[i][j] <<= 8;
+    }
+  }
 }
 
 int main(int argc, char* argv[]) {
+  image *i1 = charger_image("quelle-est-la-meilleure-equipe-de-go.pgm");
+  demasquer(i1);
+  sauvegarder_image(i1, "kiki.pgm");
+
 }
