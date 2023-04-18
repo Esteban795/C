9146295f917d5bcffaa9e965bbb7c336c898909f
Esteban <estebanmandjukowski@gmail.com>
2023-04-18 16:07:18 +0200

    done with smart tic tac toe

--- TDs/2A/tp15_tic_tac_toe/tp-14.c
+++ TDs/2A/tp15_tic_tac_toe/tp-14.c
@@ -6,214 +6,224 @@
 #include "dicts.h"
 
 struct TTT {
-    int n;
     int k;
-    int* grille;
+    int n;
+    int* grid;
 };
 
 typedef struct TTT ttt;
 
-ttt* init_jeu(int k, int n){
-    ttt* jeu = malloc(sizeof(ttt));
-    jeu->grille = malloc(sizeof(int) * n);
-    for (int i = 0; i < n;i++){
-        jeu->grille[i] = 0;
+ttt* game_init(int n,int k){
+    ttt* game = malloc(sizeof(ttt));
+    game->k = k;
+    game->n = n;
+    game->grid = malloc(sizeof(int) * n * n);
+    for (int i = 0; i < n * n;i++){
+        game->grid[i] = 0;
     }
-    jeu->k = k;
-    jeu->n = n;
-    return jeu;
+    return game;
 }
 
-void liberer_jeu(ttt* jeu){
-    free(jeu->grille);
-    free(jeu);
+void game_free(ttt* game){
+    free(game->grid);
+    free(game);
 }
 
-int* repartition(ttt* jeu){
-    int* histogram = malloc(sizeof(int) * 3);
-    for (int i = 0; i < 3;i++){
-        histogram[i] = 0;
+int* histogram(ttt* game){
+    int* hist = malloc(3 * sizeof(*hist));
+    for (int i = 0; i < 3; i++) hist[i] = 0;
+    for (int i = 0; i < game->n * game->n; i++){
+        int val = game->grid[i];
+        assert (0 <= val && val < 3);
+        hist[val] += 1;
     }
-    for (int i = 0; i < jeu->n;i++){
-        histogram[jeu->grille[i]] += 1;
-    }
-    return histogram;
+    return hist;
 }
 
-int joueur_courant(ttt* jeu){
-    int* histogram = repartition(jeu);
-    if (histogram[0] == 0) {
-        free(histogram);
-        return 0;
-    }
-    if (histogram[1] > histogram[2]) {
-        free(histogram);
-        return 2;
-    }
-    free(histogram); 
-    return 1;
+/*
+Player 1 plays first.
+So if both players played the same number of times, 
+it is now p1's turn.
+*/
+int current_player(ttt* game){
+    int* hist = histogram(game);
+    int res;
+    if (hist[0] == 0) res = 0;
+    else if (hist[1] == hist[2]) res = 1;
+    else res = 2;
+    free(hist);
+    return res;
 }
 
-void jouer_coup(ttt* jeu, int lgn, int cln){
-    int cp = joueur_courant(jeu);
-    if (cp == 0) return EXIT_FAILURE;
-    int index = jeu->n * lgn + cln;
-    if (jeu->grille[index] != 0) return EXIT_FAILURE;
-    jeu->grille[index] = cp;
+void play(ttt* game, int line, int column) {
+    int i = line * game->n + column;
+    if (game->grid[i] != 0) {
+        printf("Something is already placed here. Pick another case");
+    } else {
+        game->grid[i] = current_player(game);
+    }
 }
 
-bool alignement(ttt* jeu, int i, int di, int joueur){
-    int k = jeu->k;
-    int n = jeu->n;
-    int ligne = i % n;
-    int colonne = i / n;
+bool winning_lining_up(ttt* game,int i,int di,int player){
+    int k = game->k;
+    int n = game->n;
+
+    int column = i % n;
+    int line = i / n;
+
+    int dc = ((di + 1) % n) - 1;
     int dl = (di + 1) / n;
-    int dc = (di + 1) % n - 1;
-    for (int j = 0; j < jeu->k;j++){
-        if (ligne < 0 || ligne >= n || colonne < 0 || colonne >= n) return false;
-        i = ligne * jeu->n + colonne;
-        if (jeu->grille[i] != joueur){
-            return false;
-        }
-        colonne += dc;
-        ligne += dl;
+    for (int j = 0; j < k; j++){
+        if (line < 0 || line >= n || column < 0 || column >= n) return false;
+        i = line * n + column;
+        if (game->grid[i] != player) return false;
+        column += dc;
+        line += dl;
     }
     return true;
 }
 
-bool gagnant(ttt* jeu, int joueur){
-    int n = jeu->n;
-    int dep[] = {n, n - 1, 1, n + 1};
-    for (int i = 0; i < n * n;i++){
+bool winner(ttt* game,int player){
+    int n = game->n;
+    int arr_di[4] = {1, n - 1, n, n + 1};
+    for (int i = 0; i < n * n; i++){
         for (int j = 0; j < 4; j++){
-            if (alignement(jeu,i,dep[j],joueur)) return true;
+            if (winning_lining_up(game,i,arr_di[j],player)) return true;
         }
     }
     return false;
 }
 
-int encodage(ttt* jeu){
-    int nb = 0;
-    for (int i = 0; i < jeu->n * jeu->n;i++){
-        nb = 3 * nb + jeu->grille[i];
+
+/*
+Every game can be encoded as an integer with n² digits in base 3. Efficient storage.
+*/
+int encoding(ttt* game){
+    int key = 0;
+    int n = game->n;
+    for (int i = 0; i < n * n; i++){
+        key = 3 * key + game->grid[i];
     }
-    return nb;
+    return key;
 }
 
-int attracteur(ttt* jeu, dict* d){
-    int encoded = encodage(jeu);
-    int n = jeu->n;
-    if (!member(d,encoded)) {
-        if (gagnant(jeu,1)) add(d,encoded,1);
-        else if (gagnant(jeu,2)) add(d,encoded,2);
+/*
+3 - player is the other player
+*/
+int attracts(ttt* game, dict* d){
+    int key = encoding(game);
+    int n = game->n;
+    int player = current_player(game);
+    if (!member(d,key)) {
+        if (winner(game,player) ) add(d,key,player);
+        else if (winner(game,3 - player)) add(d,key,3 - player);
+        else if (player == 0) add(d,key,0);
         else {
-            int tab_attr[] = {0,0,0};
-            int current_player = joueur_courant(jeu);
-            for (int i = 0;i < n * n; i++){
-                if (jeu->grille[i] == 0){
-                    jeu->grille[i] = current_player;
-                    int attr = attracteur(jeu,d);
-                    jeu->grille[i] = 0;
-                    if (attr == current_player) break;
+            int tab_attracts[3] = {0,0,0};
+            for (int i = 0; i < n * n; i++){
+                if (game->grid[i] == 0) {
+                    game->grid[i] = player;
+                    int att = attracts(game,d);
+                    game->grid[i] = 0;
+                    tab_attracts[att] += 1;
+                    if (att == player) break;
                 }
             }
-            if (tab_attr[current_player] > 0) add(d,encoded,current_player);
-            if (tab_attr[0] > 0) add(d,encodage,0); 
-            if (tab_attr[3 - current_player] > 0) add(d,encoded,3 - current_player);
+            if (tab_attracts[player] != 0) add(d,key,player);
+            else if (tab_attracts[0] != 0) add(d,key,0);
+            else add(d,key,3 - player);
         }
     }
-    return get(d,encoded);
+    return get(d,key);
 }
 
-int strategie_optimale(ttt* jeu, dict* d){
-    int attr = attracteur(jeu,d);
-    int joueur = joueur_courant(jeu);
-    for (int i = 0; i < jeu->n * jeu->n;i++){
-        if (jeu->grille[i] == 0){
-            jeu->grille[i] = joueur;
-            int next_attr = attracteur(jeu,d);
-            jeu->grille[i] = 0;
-            if (attr == next_attr) return i;
+int optimal_strat(ttt* game,dict* d){
+    int att = attracts(game,d);
+    int n = game->n;
+    int player = current_player(game);
+    for (int i = 0; i < n * n; i++) {
+        if (game->grid[i] == 0) {
+            game->grid[i] = player;
+            int att2 = attracts(game, d);
+            game->grid[i] = 0;
+            if (att == att2) return i;
         }
     }
-    return -1;
 }
 
 
-void print_sep(ttt* jeu){
-    printf("+");
-    for (int i = 0; i < jeu->n;i++){
-        printf("-+");
+void display_line_sep(int n){
+    printf("\n ");
+    for (int i = 0; i < n; i++){
+        printf("+-");
+    }
+    printf("+\n");
+}
+
+void display(ttt* jeu){
+    int n = jeu->n;
+    char arr[3] = {' ', 'X', 'O'};
+    printf(" ");
+    for (int cln = 0; cln < n; cln++){
+        printf(" %d", cln);
+    }
+    display_line_sep(n);
+    for (int lgn = 0; lgn < n; lgn++){
+        printf("%d|", lgn);
+        for (int cln = 0; cln < n; cln++){
+            int i = n * lgn + cln;
+            printf("%c|", arr[jeu->grid[i]]);
+        }
+        display_line_sep(n);
     }
     printf("\n");
 }
 
-void print_ligne(ttt* jeu){
-    for (int i = 0; i < jeu->n;i++){
-        if (jeu->grille[i] == 0) continue;
-        if (jeu->grille[i] == 1) printf(" 0 |");
-        if (jeu->grille[i] == 2) printf(" X |");
-    }
-    printf("\n");
-}
-
-void afficher(ttt* jeu){
-    for (int i = 0; i < jeu->n;i++){
-        printf("%d ",i);
-    }
-    print_sep(jeu);
-    for (int i = 0; i < jeu->n;i++){
-        printf("%d |",i);
-        print_ligne(jeu->grille + jeu->n * i);
-    }
-    print_sep(jeu);
-}
-
-void jouer_partie(int k, int n){
-    ttt* jeu = init_jeu(k,n);
+void game_play(int k, int n){
+    ttt* game = game_init(k,n);
     char c;
-    int IA,cln,lgn;
+    int bot ,line,column;
     while (true) {
-        printf("Voulez-vous commencer ? (o/n)\n");
+        printf("Would you like to play first (y/n)\n");
         scanf("%c",&c);
-        if (c == 'o'){
-            IA = 2;
+        if (c == 'y'){
+            bot = 2;
             break;
         } else if (c == 'n') {
-            IA = 1;
+            bot = 1;
             break;
         }
     }
     dict* d = create();
-    int joueur = 1;
-    while (joueur != 0 & !gagnant(jeu,1) && !gagnant(jeu,2)){
-        afficher(jeu);
-        if (joueur == IA){
-            int i = strategie_optimale(jeu,d);
-            cln = i%n;
-            lgn = i/n;
-            printf("Le bot joue ligne %d et colonne %d\n",lgn,cln);
-            jouer_coup(jeu,cln,lgn);
+    int player = 1;
+    while (player != 0 && !winner(game,1) && !winner(game,2)){
+        display(game);
+        if (player == bot){
+            int i = optimal_strat(game,d);
+            column = i % n;
+            line = i / n;
+            printf("Bot plays line %d and columns %d\n",line,column);
+            play(game,line,column);
         } else {
-            printf("A vous de jouer\n");
-            printf("Saisir ligne colonne :\n");
-            scanf("%d %d",&lgn,&cln);
-            if (cln  < 0 || cln >= jeu->n || lgn < 0 || lgn > jeu->n){
+            printf("Your turn to play\n Type line column :\n");
+            scanf("%d %d",&line,&column);
+            if (column  < 0 || column >= game->n || line < 0 || line >= game->n){
                 printf("Out of bounds");
             } else {
-                jouer_coup(jeu,cln,lgn);
+                play(game,line,column);
             }
         }
-        joueur = joueur_courant(jeu);
+        player = current_player(game);
     }
-    afficher(jeu);
-    if (gagnant(jeu,IA)) printf("Le bot a gagné");
-    else if (gagnant(jeu,3 - IA)) printf("Vous avez gagné");
-    else printf("match nul, chanceux");
-    liberer_jeu(jeu);
+    display(game);
+    if (winner(game,bot)) printf("The bot won.");
+    else if (winner(game,3 - bot)) printf("You actually won.");
+    else printf("Even, quite surprising.");
+    game_free(game);
     dict_free(d);
 }
 
 int main(void){
+    game_play(4,4);
     return 0;
 }
